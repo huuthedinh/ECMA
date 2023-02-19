85485096c7febf7e2c63a618c0fd9370c6277bb0
Thế Đinh <thedh081003@gmail.com>
2023-02-20 00:51:03 +0700

    add img

--- ASM/pages/admin/projects-add.js
+++ ASM/pages/admin/projects-add.js
@@ -1,6 +1,7 @@
 import { useEffect, router } from "../../lib"
 
 import header from "../../components/header";
+import axios from "axios";
 
 
 const AdminAddProjectsPage = () => {
@@ -8,18 +9,22 @@ const AdminAddProjectsPage = () => {
         const form = document.getElementById("form-add");
         const projectName = document.getElementById("project-name");
         const projectdes = document.getElementById("project-des");
-        const projectimg = document.getElementById("project-img");
+        const projectImg = document.getElementById("project-img");
         const projecttext = document.getElementById("project-text");
 
 
-        form.addEventListener("submit", function (e) {
+        form.addEventListener("submit", async function (e) {
             // chặn sự kiện reload sau khi submit
             e.preventDefault();
-            // Thêm phần tử vào mảng projects
-            const formData = {
+
+            const urls = await uploadFiles(projectImg.files);
+            //Thêm phần tử vào mảng projects
+
+            const formProjects = {
                 name: projectName.value,
                 des: projectdes.value,
                 text: projecttext.value,
+                img: urls,
 
             };
             fetch("http://localhost:3000/projects", {
@@ -27,13 +32,37 @@ const AdminAddProjectsPage = () => {
                 headers: {
                     "Content-Type": "application/json",
                 },
-                body: JSON.stringify(formData),
+                body: JSON.stringify(formProjects),
 
             }).then(() => router.navigate("/admin/projects"))
         });
-
-
     });
+    const uploadFiles = async (files) => {
+        if (files) {
+            const CLOUD_NAME = "dvg4a5rbv";
+            const PRESET_NAME = "demo_upload";
+            const FOLDER_NAME = "ECMA";
+            const urls = [];
+            const api = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`;
+
+            const formData = new FormData();
+
+            formData.append("upload_preset", PRESET_NAME);
+            formData.append("folder", FOLDER_NAME);
+
+            for (const file of files) {
+                formData.append("file", file);
+                const response = await axios.post(api, formData, {
+                    headers: {
+                        "Content-Type": "multipart/form-data"
+                    },
+
+                });
+                urls.push(response.data.secure_url);
+            }
+            return urls;
+        }
+    };
     return /*html*/ `<div class ="container">
     ${header()}
     <h1>Thêm Sản Phẩm</h1>
@@ -46,7 +75,7 @@ const AdminAddProjectsPage = () => {
          <label for="" class="form-lable">Ngôn Ngữ Sử Dụng<label>
          <input type="text" class="form-control" id="project-text">
          <label for="" class="form-lable">Ảnh Dự Án<label>
-         <input type="file" class="form-control" id="project-img">
+         <input type="file" class="form-control" multiple id="project-img">
       </div>
       <br>
       <button class="btn btn-primary">Thêm</button>
